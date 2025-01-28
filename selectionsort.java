f7a6566b97bcec1a5be362b5cc4503bdf9ceb2a9
shubhamm008 <shubhamsinghjohn50@gmail.com>
2025-01-28 14:30:54 +0530

    searching techniques

---
+++ selectionsort.java
@@ -0,0 +1,50 @@
+public class selectionsort {
+    public static void selectionsortt(int arr[])
+    {
+        int i=0;
+        // System.out.println(arr.length);
+        while(i<arr.length)
+        {
+            int ind=i;
+            for(int j=i;j<arr.length;j++)
+            {
+                if(arr[j]<arr[ind])
+                {
+                    ind=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[ind];
+            arr[ind]=temp;
+            i++;
+        }
+        for(i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void insertionsort(int arr[])
+    {
+        int i=0;
+        int j;
+        while(i<arr.length)
+        {
+            int element=arr[i];
+            for(j=i-1;j>=0 && arr[j]>element;j--)
+            {
+                arr[j+1]=arr[j];
+            }
+            arr[j+1]=element;
+            i++;
+        }
+        for(int k=0;k<arr.length;k++)
+        {
+            System.out.print(arr[k] +" ");
+        }
+    }
+    public static void main(String[] args) 
+    {
+        int arr[]={10,9,8,7,6};
+        insertionsort(arr);
+    }
+}
